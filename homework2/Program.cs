805fe7ac9cfdfdd5744a866377f773eccfde7c43
Maks <nordmax69@gmail.com>
2022-09-14 18:12:37 +0700

    added homework4

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -1,23 +1,18 @@
-﻿// В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
+// В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-int [,] massive = new int [5,7];
-int minelement = 0;
-int indexminelementstring = 0 ,indexminelementcolumn = 0;
-for (int i = 0; i < massive.GetLength(0); i++)
+
 {
     for (int j = 0; j < massive.GetLength(1); j++)
     {
-        massive[i,j] = new Random().Next(20);
-        Console.Write("{0}  ",massive[i,j]);
-    }
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1,10);
+            Console.Write("{0}  ",arr[i,j]);
 }
 Console.WriteLine();
 
 
-Console.WriteLine(new String('*',10));
-Console.WriteLine();
-
-for (int i = 0; i < massive.GetLength(0); i++)
+string FindMinElement(int [,] arr)
 {
     for (int j = 0; j < massive.GetLength(1); j++)
     {
